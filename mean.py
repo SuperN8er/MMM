04aee19923ff0331a44dd35770f046493fc03f9b
Nathaniel Douglas <n8douglas@gmail.com>
2021-06-13 20:58:53 -0400

    wrote pseudocode for mean functionality

---
+++ mean.py
@@ -0,0 +1,21 @@
+"""Module for working with mean"""
+
+
+"""
+given a list of numbers
+calc the mean of the list
+
+add all numbers on list and store in var
+find len of list and store in var
+
+divide the sum by len
+
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
